25755cc5ae3f96f52cd89b3a9bf4ebb7645587d6
kathmandhu <70630924+kathmandhu@users.noreply.github.com>
2024-11-12 16:20:33 +0545

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Ensures Tailwind processes all files in /src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
