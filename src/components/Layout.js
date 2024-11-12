975f7c8e364c6bd7975dd30c855eedcf066e29a3
kathmandhu <70630924+kathmandhu@users.noreply.github.com>
2024-11-12 16:20:50 +0545

    Create Layout.js

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import Header from './Header'
+import Footer from './Footer'
+import MobileFooterMenu from './MobileFooterMenu'
+
+export default function Layout({ children }) {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-grow w-full">{children}</main>
+            <MobileFooterMenu />
+            <Footer />
+        </div>
+    )
+}
