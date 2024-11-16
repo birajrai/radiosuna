64e9fa9b141c10dad2774553fd08f8aff97f23f0
kathmandhu <70630924+kathmandhu@users.noreply.github.com>
2024-11-16 11:35:10 +0545

    Create stream.js

---
+++ src/pages/api/stream.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+    try {
+        const response = await fetch('https://backend.bishestamedia.com.np/api/stream.php');
+
+        // Check if the response is JSON
+        if (response.headers.get('content-type')?.includes('application/json')) {
+            const data = await response.json();
+            res.status(200).json(data);
+        } else {
+            throw new Error('External API did not return JSON');
+        }
+    } catch (error) {
+        console.error('Error in API route:', error.message);
+        res.status(500).json({ message: 'Failed to fetch data from external API', error: error.message });
+    }
+}
