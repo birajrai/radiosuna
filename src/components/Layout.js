8c6879d7f535e2899f65020a28361cf49352fb42
kathmandhu <70630924+birajrai@users.noreply.github.com>
2024-11-28 16:32:39 +0545

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,14 @@
 import Header from './Header'
-import Footer from './Footer'
-import MobileFooterMenu from './MobileFooterMenu'
+// import Footer from './Footer'
+// import MobileFooterMenu from './MobileFooterMenu'
 
 export default function Layout({ children }) {
     return (
         <div className="min-h-screen flex flex-col">
             <Header />
             <main className="flex-grow w-full">{children}</main>
-            <MobileFooterMenu />
-            <Footer />
+            {/* <MobileFooterMenu /> */}
+            {/* <Footer /> */}
         </div>
     )
 }
